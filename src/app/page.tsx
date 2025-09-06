09334789dfdff5c78b777a10bb45ec3f1f855592
ParsaProg <parsashaabani3@gmail.com>
2025-09-06 15:57:13 +0330

    test chore: page.tsx (home page)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function HomePage(){
-  return <div className=""></div>
+export default function HomePage() {
+  return (
+    <div className="text-red-500">
+
+      <h2>Hello</h2>
+    </div>
+  );
 }
