4401b61c77a7bd954b5d19069f983692c7aa70f5
ParsaProg <parsashaabani3@gmail.com>
2025-09-07 01:22:42 +0330

    main layout.ts file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning>
-      <body className=" dark:bg-background-dark bg-background-light">
+      <body className="select-none dark:bg-background-dark bg-background-light">
         <LangProvider>
           <ThemeProvider enableSystem defaultTheme="system" attribute="class">
             <ClientLayout>{children}</ClientLayout>
