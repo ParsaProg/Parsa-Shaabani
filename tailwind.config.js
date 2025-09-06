61a7237d4ec7f3023ecbbd516a7d3afaf781d51d
ParsaProg <parsashaabani3@gmail.com>
2025-09-06 15:54:30 +0330

    tailwind config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // فعال کردن darkMode با کلاس
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { dark: "#FF7E3D", light: "#4D6BFE" },
+        background: { dark: "#09090B", light: "#FFFFFF" },
+      },
+    },
   },
   plugins: [],
-}
-
+};
