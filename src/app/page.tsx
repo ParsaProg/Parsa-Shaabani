757a21112deafbeeee2daf41fa959f3e5c0bf298
ParsaProg <parsashaabani3@gmail.com>
2025-09-07 13:27:27 +0330

    responsive sections landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MySkills from "@/components/landing/Skills";
 
 export default function HomePage() {
   return (
-    <div className="mt-[90px] w-[90%] mx-auto">
+    <div className="mt-[90px] md:w-[90%] w-full mx-auto">
       <HeroSection />
       <MySkills />
     </div>
