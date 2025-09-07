e74b01871acb6f9c276fe9bc29b2aca960b0bee7
ParsaProg <parsashaabani3@gmail.com>
2025-09-07 13:15:35 +0330

    page.tsx home page added hero end and top skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/landing/HeroSection";
+import MySkills from "@/components/landing/Skills";
 
 export default function HomePage() {
   return (
-    <div className="mt-[70px]">
+    <div className="mt-[90px]">
       <HeroSection />
+      <MySkills />
     </div>
   );
 }
