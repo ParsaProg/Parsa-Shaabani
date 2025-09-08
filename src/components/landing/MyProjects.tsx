89fd58e19c8d92404c3980360cff0e36572c50d3
ParsaProg <parsashaabani3@gmail.com>
2025-09-08 17:39:21 +0330

    my projects section updates

---
+++ src/components/landing/MyProjects.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useLang } from "@/contexts/languegeContext";
+import en from "@/langs/en.json";
+import fa from "@/langs/fa.json";
+import { BriefcaseBusiness, Github } from "lucide-react";
+import ProjectContainer from "../ui/ProjectsContainer";
+import {motion} from "framer-motion";
+import {
+  ProjectsModelDataEn,
+  ProjectsModelDataFa,
+} from "@/models/projects-model";
+
+export default function MyProjects() {
+  const { lang } = useLang();
+  return (
+    <div className="[@media(max-width:1200px)]:w-[95%] w-[90%] mx-auto overflow-hidden">
+      <div className="flex dark:text-primary-dark text-primary-light items-center gap-x-2">
+        <BriefcaseBusiness size={24} />
+        <h1 className="dark:text-white text-black font-bold text-[25px]">
+          {lang === "en" ? en.myProjects.title : fa.myProjects.title}
+        </h1>
+      </div>
+      <div className="mt-5 grid [@media(min-width:1240px)]:grid-cols-3 grid-cols-2 gap-5 w-auto [@media(max-width:850px)]:grid-cols-1">
+        {lang === "en"
+          ? ProjectsModelDataEn.map((val, index) => {
+              return <ProjectContainer key={index} {...val} />;
+            })
+          : ProjectsModelDataFa.map((faVal, faIndex) => {
+              return <ProjectContainer key={faIndex} {...faVal} />;
+            })}
+      </div>
+      <div className="mt-10 flex flex-col items-center">
+        <motion.a href="https://github.com/ParsaProg?tab=repositories" target="_blank" whileTap={{
+          scale: 0.95
+        }} className="rounded-full dark:bg-neutral-800 bg-slate-200 py-3 px-5 flex items-center gap-x-2 justify-center">
+        <Github size={18}/>
+        {lang === "en" ? en.myProjects.seeMore : fa.myProjects.seeMore}
+      </motion.a>
+      </div>
+    </div>
+  );
+}
