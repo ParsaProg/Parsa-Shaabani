3c93db7fd1fc8cffbc4060c4ebbbf124dd962a56
ParsaProg <parsashaabani3@gmail.com>
2025-09-12 18:53:43 +0330

    fake gallery data file

---
+++ src/models/fakeGalleryData.ts
@@ -0,0 +1,125 @@
+// data/galleryData.ts
+import { GalleryItem } from "@/types/GalleryItems";
+
+export const galleryData: GalleryItem[] = [
+  {
+    picture: "https://api2.zoomg.ir/media/f2a3i4kwwt9gspsvkkrmbb-67658977bc142138f8158672?w=1920&q=80",
+    age: 0,
+    enCategory: "Technology",
+    faCategory: "تکنولوژی",
+    farsiTitle: "نسل جدید لپ‌تاپ‌های Asus",
+    englishTitle: "The new generation of Asus laptops",
+    date: "2025/11/25",
+    faDesc: "نسل جدید لپتاپ‌های مدرن ایسوس با کارت گرافیک غول 5090 عرضه شده‌اند",
+    enDesc: "The new generation of Asus laptops publish the monster 5090 GPU",
+    likes: 20
+  },
+  {
+    picture: "https://falnic.com/blog/wp-content/uploads/2020/06/best-virtual-classroom-2020-falnic.jpg.webp",
+    age: 1,
+    enCategory: "Education",
+    faCategory: "تحصیلی",
+    farsiTitle: "روش‌های نوین آموزش آنلاین",
+    englishTitle: "Modern methods of online education",
+    date: "2025/10/12",
+    faDesc: "پلتفرم‌های جدید یادگیری آنلاین امکانات بیشتری برای دانشجویان فراهم کرده‌اند",
+    enDesc: "New online learning platforms provide more opportunities for students",
+    likes: 34
+  },
+  {
+    picture: "https://cdn.khabarfoori.com/thumbnail/ggF8xdPvqwba/KJT939w3udAFHFrusS9hOW5pwW1UhH2adWYXx4humaDkM_kfgLRUFplbxHcNmK5EDHRCtvWzWpXUUqvoCoPEioKndrf2ISQJMIUb70m_O-HJaJytMB09UIIgBzv2wppq/63540286.jpg",
+    age: 2,
+    enCategory: "Scientific",
+    faCategory: "علمی",
+    farsiTitle: "کشف سیاره‌ای جدید در منظومه شمسی",
+    englishTitle: "A new planet discovered in the solar system",
+    date: "2025/09/20",
+    faDesc: "ستاره‌شناسان سیاره‌ای جدید با ویژگی‌های عجیب را کشف کردند",
+    enDesc: "Astronomers have discovered a new planet with unusual features",
+    likes: 51
+  },
+  {
+    picture: "https://static.farakav.com/files/pictures/01081378.jpg",
+    age: 3,
+    enCategory: "Sports",
+    faCategory: "ورزشی",
+    farsiTitle: "قهرمانی تیم ملی فوتبال ایران",
+    englishTitle: "Iran national football team wins championship",
+    date: "2025/08/05",
+    faDesc: "تیم ملی فوتبال ایران در مسابقات آسیایی قهرمان شد",
+    enDesc: "Iran national football team became the champion of the Asian Cup",
+    likes: 72
+  },
+  {
+    picture: "https://www.chetor.com/wp-content/uploads/2018/08/134815.jpg",
+    age: 4,
+    enCategory: "Private Life",
+    faCategory: "زندگی شخصی",
+    farsiTitle: "۱۰ نکته برای کاهش استرس روزانه",
+    englishTitle: "10 tips to reduce daily stress",
+    date: "2025/07/15",
+    faDesc: "با استفاده از این روش‌ها می‌توانید آرامش بیشتری در زندگی داشته باشید",
+    enDesc: "These techniques help you to have more peace in daily life",
+    likes: 60
+  },
+  {
+    picture: "https://static.digiato.com/digiato/2020/02/Smart_ways_to_break_your_social_media_addiction-1024x681.jpg",
+    age: 5,
+    enCategory: "Social relations",
+    faCategory: "روابط اجتماعی",
+    farsiTitle: "تأثیر شبکه‌های اجتماعی بر روابط دوستانه",
+    englishTitle: "The effect of social media on friendships",
+    date: "2025/06/30",
+    faDesc: "شبکه‌های اجتماعی نحوه ارتباط مردم را تغییر داده‌اند",
+    enDesc: "Social media has changed the way people communicate",
+    likes: 45
+  },
+  {
+    picture: "https://neuronclinic.ir/wp-content/uploads/2024/11/770418039700135589-1-1024x683-1.png",
+    age: 6,
+    enCategory: "Technology",
+    faCategory: "تکنولوژی",
+    farsiTitle: "رشد سریع هوش مصنوعی در پزشکی",
+    englishTitle: "The rapid growth of AI in medicine",
+    date: "2025/05/22",
+    faDesc: "هوش مصنوعی به سرعت در تشخیص و درمان بیماری‌ها استفاده می‌شود",
+    enDesc: "AI is rapidly being used in diagnosis and treatment of diseases",
+    likes: 80
+  },
+  {
+    picture: "https://sharifstudy.com/wp-content/uploads/2023/04/Ranking-of-world-universities.1.webp",
+    age: 7,
+    enCategory: "Education",
+    faCategory: "تحصیلی",
+    farsiTitle: "برترین دانشگاه‌های جهان در سال ۲۰۲۵",
+    englishTitle: "Top universities in the world 2025",
+    date: "2025/04/10",
+    faDesc: "رتبه‌بندی جدید بهترین دانشگاه‌های جهان منتشر شد",
+    enDesc: "The new ranking of the world's best universities has been released",
+    likes: 25
+  },
+  {
+    picture: "https://www.citna.ir/sites/default/files/styles/large/public/1403-09/44_337.jpg?itok=NZGWO4_X",
+    age: 8,
+    enCategory: "Sports",
+    faCategory: "ورزشی",
+    farsiTitle: "رکوردشکنی دونده ایرانی در المپیک",
+    englishTitle: "Iranian runner breaks record in Olympics",
+    date: "2025/03/18",
+    faDesc: "ورزشکار ایرانی موفق به ثبت رکورد جهانی جدید شد",
+    enDesc: "Iranian athlete set a new world record in Olympics",
+    likes: 90
+  },
+  {
+    picture: "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9613/production/_115691483_e9c5dbe5-599b-4033-b01f-9eeee1e2381c.jpg.webp",
+    age: 9,
+    enCategory: "Scientific",
+    faCategory: "علمی",
+    farsiTitle: "واکسن جدید برای بیماری‌های نادر",
+    englishTitle: "New vaccine for rare diseases",
+    date: "2025/02/05",
+    faDesc: "پژوهشگران موفق به ساخت واکسن جدید برای درمان بیماری‌های نادر شدند",
+    enDesc: "Researchers developed a new vaccine for rare diseases",
+    likes: 39
+  }
+];
