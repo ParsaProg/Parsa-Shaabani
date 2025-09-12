f8d012640b95cfb1d828920512be230a995a92bf
ParsaProg <parsashaabani3@gmail.com>
2025-09-12 18:53:56 +0330

    gallery items.ts file

---
+++ src/types/GalleryItems.ts
@@ -0,0 +1,13 @@
+
+export interface GalleryItem {
+  picture: string;
+  age: number;
+  enCategory: string;
+  faCategory: string;
+  farsiTitle: string;
+  englishTitle: string;
+  date: string;
+  faDesc: string;
+  enDesc: string;
+  likes: number;
+}
