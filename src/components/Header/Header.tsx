d3fd4ebaf9d40ed95812a99c11b64cdc8ce03d20
ParsaProg <parsashaabani3@gmail.com>
2025-09-13 16:28:53 +0330

    Header.tsx chnages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
   const { toggleLang, lang } = useLang();
   const { theme, setTheme } = useTheme();
   return (
-    <div className="fixed z-[99999] top-0 dark:bg-background-dark bg-background-light w-full p-2 flex justify-center border-b-[1px] dark:border-b-[#27272A] border-b-[#E2E8F0]">
+    <div className="fixed z-[99999] top-0 dark:bg-[#09090bd2] backdrop-blur-xl bg-[#ffffffde] w-full p-2 flex justify-center border-b-[1px] dark:border-b-[#27272A] border-b-[#E2E8F0]">
       <div className=" [@media(max-width:1200px)]:w-[95%] w-[80%] flex items-center justify-between py-3">
         <a href="/" className="w-[100px]">
           <svg
