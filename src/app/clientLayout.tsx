848eea9c60f00886fd2f1cd9559e72dbc280c42b
ParsaProg <parsashaabani3@gmail.com>
2025-09-30 17:33:34 +0330

    client layout add toastProvider

--- src/app/clientLayout.tsx
+++ src/app/clientLayout.tsx
@@ -5,8 +5,9 @@ import Header from "@/components/Header/Header";
 import TopScrollButton from "@/components/ui/TopScrollButton";
 import { useLang } from "@/contexts/languageContext";
 import SmoothScrollLayout from "@/lib/smooth-scroll";
+import { ToastProvider } from "@/providers/react-hot-toast-provider";
 import Lenis from "@studio-freight/lenis/types";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 
 export const ClientLayout = ({ children }: { children: ReactNode }) => {
   const { lang } = useLang(); // get current languageAnimationFrame(raf);
@@ -16,8 +17,10 @@ export const ClientLayout = ({ children }: { children: ReactNode }) => {
       className={`${lang === "fa" ? "font-iransans" : "font-inter"}`}
       dir={lang === "fa" ? "rtl" : "ltr"}
     >
+      <ToastProvider />
       <TopScrollButton />
       <Header />
+
       <div className="h-[50px]"></div>
       <SmoothScrollLayout>{children}</SmoothScrollLayout>
       <Footer />
