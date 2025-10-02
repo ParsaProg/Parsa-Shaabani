a7424c899cca1e435f6cfcfbf79f7c71c4e45584
ParsaProg <parsashaabani3@gmail.com>
2025-10-02 15:14:34 +0330

    Header.tsx  main changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,16 +5,12 @@ import ChangeLang from "./lang/changeLang";
 import ConnectionDialog from "./connection/ConnectionDialog";
 import Link from "next/link";
 import MobileMenuDialog from "./mobile/MenuDialog";
-import {motion} from "framer-motion";
-import { Github, Linkedin, MailIcon, PhoneCall } from "lucide-react";
-import en from "@/langs/en.json";
-import fa from "@/langs/fa.json"
 
 export default function Header() {
   const { toggleLang, lang } = useLang();
   const { theme, setTheme } = useTheme();
   return (
-    <div className="fixed z-[99999] top-0 dark:bg-[#09090bd2] backdrop-blur-xl bg-[#ffffffde] w-full p-2 flex justify-center border-b-[1px] dark:border-b-[#27272A] border-b-[#E2E8F0]">
+    <div className="fixed z-[99999] top-0 dark:bg-background-dark  backdrop-blur-xl bg-[#ffffffde] w-full p-2 flex justify-center border-b-[1px] dark:border-b-[#3e3d3d] border-b-[#E2E8F0]">
       <div className=" [@media(max-width:1200px)]:w-[95%] w-[80%] flex items-center justify-between py-3">
         <a href="/" className="w-[100px]">
           <svg
