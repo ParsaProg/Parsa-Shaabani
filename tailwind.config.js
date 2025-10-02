fedf4b251ba1ea45a22b2235a659fdd2bb7bc5e1
ParsaProg <parsashaabani3@gmail.com>
2025-10-02 15:14:05 +0330

    add modern animation-morph on the hero section, tailwind config add main animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // فعال کردن darkMode با کلاس
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
         primary: { dark: "#FF7E3D", light: "#4D6BFE" },
         background: { dark: "#09090B", light: "#FFFFFF" },
+      },
+      animation: {
+        "morph-radius": "morphRadius 8s ease-in-out infinite",
+      },
+      keyframes: {
+        morphRadius: {
+          "0%, 100%": {
+            borderRadius: "30% 70% 70% 30% / 30% 30% 70% 70%",
+          },
+          "25%": {
+            borderRadius: "58% 42% 75% 25% / 76% 46% 54% 24%",
+          },
+          "50%": {
+            borderRadius: "50% 50% 33% 67% / 55% 27% 73% 45%",
+          },
+          "75%": {
+            borderRadius: "33% 67% 58% 42% / 63% 68% 32% 37%",
+          },
+        },
       },
     },
   },
